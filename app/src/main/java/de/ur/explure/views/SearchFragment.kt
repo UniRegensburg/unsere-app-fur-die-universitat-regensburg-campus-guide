ef610add2f334a50f5bc4ec1f24071e28dc69c98
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-01-18 15:59:37 +0100

    Add fragment for each app section

---
+++ app/src/main/java/de/ur/explure/views/SearchFragment.kt
@@ -0,0 +1,19 @@
+package de.ur.explure.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.ur.explure.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
