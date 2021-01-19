bb0ed2a03803c3228b4655dad2f951ca8b03a35f
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2021-01-19 22:47:12 +0100

    Cleanup test files and remove wrong import

--- app/src/main/java/de/ur/explure/ExplureApp.kt
+++ app/src/main/java/de/ur/explure/ExplureApp.kt
@@ -1,7 +1,6 @@
 package de.ur.explure
 
 import android.app.Application
-import de.ur.campusguide.BuildConfig
 import de.ur.explure.di.mainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
