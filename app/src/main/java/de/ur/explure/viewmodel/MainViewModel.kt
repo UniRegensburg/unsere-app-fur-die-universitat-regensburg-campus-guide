f6e991a9c509492665219a5813e848e88ea67620
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-01-23 17:54:41 +0100

    Initialize navigation in MainActivity by using navigation component extensions which allow multiple nav graphs in bottom navigation

--- app/src/main/java/de/ur/explure/viewmodel/MainViewModel.kt
+++ app/src/main/java/de/ur/explure/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package de.ur.explure.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import de.ur.explure.navigation.AppRouter
@@ -12,8 +13,41 @@ import de.ur.explure.navigation.AppRouter
 
 class MainViewModel(private val appRouter: AppRouter) : ViewModel() {
 
-    fun setNavigationGraph(navController: NavController) {
+    lateinit var currentNavController: LiveData<NavController>
+
+    /**
+     * Sets new observable navigation controller
+     *
+     * @param [LiveData]<[NavController]> object with the new navigation controller which should be observed
+     */
+
+    fun setNavigationController(navController: LiveData<NavController>) {
+        currentNavController = navController
+        setNavigationGraphInAppRouter()
+    }
+
+    /**
+     * Sets navigation graph in AppRouter
+     *
+     */
+
+    private fun setNavigationGraphInAppRouter() {
+        val navController = currentNavController.value ?: return
         appRouter.setGraph(navController)
+    }
+
+    /**
+     * Navigate ups in the current navigation controller.
+     *
+     * @return Returns [Boolean] value = true if backstack is not empty | false if backstack is empty.
+     */
+
+    fun navigateUp(): Boolean {
+        return if (this::currentNavController.isInitialized) {
+            currentNavController.value?.navigateUp() ?: false
+        } else {
+            false
+        }
     }
 
 }
