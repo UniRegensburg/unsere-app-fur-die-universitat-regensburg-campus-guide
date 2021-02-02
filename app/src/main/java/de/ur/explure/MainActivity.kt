ff8a820357a1e6e2dfb2b271993dc794e2e382e7
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2021-02-02 13:49:45 +0100

    Change fragment layout to FragmentContainerView
    to follow official best practice

--- app/src/main/java/de/ur/explure/MainActivity.kt
+++ app/src/main/java/de/ur/explure/MainActivity.kt
@@ -1,8 +1,8 @@
 package de.ur.explure
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import de.ur.explure.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -21,7 +21,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initNavigation() {
-        viewModel.initializeNavController(findNavController(R.id.main_fragment_container))
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_fragment_container) as? NavHostFragment
+        val navController = navHostFragment?.navController
+        if (navController != null) {
+            viewModel.initializeNavController(navController)
+        } else {
+            throw IllegalStateException("Couldn't find associated NavController for ${this::class.java.simpleName}!")
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
