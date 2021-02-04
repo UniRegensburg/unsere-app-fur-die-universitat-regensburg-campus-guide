bac81785264c3b5af9c1ff0cef18b98e09a2d85b
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-04 12:29:55 +0100

    Add Route data model and corresponding DTO class for creation

---
+++ app/src/main/java/de/ur/explure/model/route/Route.kt
@@ -0,0 +1,24 @@
+package de.ur.explure.model.route
+
+import android.os.Parcelable
+import de.ur.explure.model.comment.Comment
+import de.ur.explure.model.landmark.Landmark
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Parcelize
+data class Route(
+    val id: String,
+    val authorId: String,
+    val category: String,
+    val date: Date,
+    val title: String,
+    val description: String,
+    val distance: Double,
+    val duration: Double,
+    val thumbnail: String?,
+    val landMarks: ArrayList<Landmark>,
+    val comments: List<Comment> = emptyList(),
+    val rating: List<String> = emptyList()
+) : Parcelable
---
+++ app/src/main/java/de/ur/explure/model/route/RouteDTO.kt
@@ -0,0 +1,26 @@
+package de.ur.explure.model.route
+
+import android.os.Parcelable
+import de.ur.explure.model.landmark.LandmarkDTO
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Parcelize
+data class RouteDTO(
+    var authorId: String,
+    var category: String,
+    var date: Date,
+    var title: String,
+    var description: String,
+    var distance: Double,
+    var duration: Double,
+    var thumbnail: String?,
+    var landMarks: ArrayList<LandmarkDTO> = ArrayList()
+) : Parcelable {
+
+    fun addLandMark(landMark: LandmarkDTO){
+        landMarks.add(landMark)
+    }
+
+}
