c8702d567ba1bf1c70d528c90b98e82ce6f21067
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-05 12:37:56 +0100

    Change parameter type in Landmark Model and add Parcelable implementation

--- app/src/main/java/de/ur/explure/model/landmark/Landmark.kt
+++ app/src/main/java/de/ur/explure/model/landmark/Landmark.kt
@@ -1,15 +1,45 @@
 package de.ur.explure.model.landmark
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import com.google.firebase.firestore.GeoPoint
 
-@Parcelize
 data class Landmark(
     val id: String,
     val title: String,
     val description: String,
-    val coordinates: String,
+    val geoPoint: GeoPoint,
     val audioURL: String?,
     val imageURL: String?,
     val videoURL: String?
-) : Parcelable
+) : Parcelable {
+
+    override fun writeToParcel(parcel: Parcel, i: Int) {
+        parcel.writeDouble(geoPoint.latitude)
+        parcel.writeDouble(geoPoint.longitude)
+    }
+
+    constructor(parcel: Parcel) : this(
+        id = parcel.readString() ?: "",
+        title = parcel.readString() ?: "",
+        description = parcel.readString() ?: "",
+        geoPoint = GeoPoint(parcel.readDouble(), parcel.readDouble()),
+        audioURL = parcel.readString(),
+        imageURL = parcel.readString(),
+        videoURL = parcel.readString(),
+    )
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Landmark> {
+        override fun createFromParcel(parcel: Parcel): Landmark {
+            return Landmark(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Landmark?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
