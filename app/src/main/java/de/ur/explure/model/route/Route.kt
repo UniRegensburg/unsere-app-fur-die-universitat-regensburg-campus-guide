54423b5163f76f5f1c8bf360a9cee6a2aa9bfcea
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-05 12:34:15 +0100

    Adjust Comment Model by renaming ,adding ServerTimestamp Annotation to creation date and changing list type

--- app/src/main/java/de/ur/explure/model/route/Route.kt
+++ app/src/main/java/de/ur/explure/model/route/Route.kt
@@ -5,20 +5,19 @@ import de.ur.explure.model.comment.Comment
 import de.ur.explure.model.landmark.Landmark
 import kotlinx.android.parcel.Parcelize
 import java.util.Date
-import kotlin.collections.ArrayList
 
 @Parcelize
 data class Route(
     val id: String,
     val authorId: String,
     val category: String,
-    val date: Date,
+    val createdAt: Date,
     val title: String,
     val description: String,
     val distance: Double,
     val duration: Double,
-    val thumbnail: String?,
-    val landMarks: ArrayList<Landmark>,
+    val thumbnailUrl: String? = null,
+    val landMarks: List<Landmark>,
     val comments: List<Comment> = emptyList(),
     val rating: List<String> = emptyList()
 ) : Parcelable
--- app/src/main/java/de/ur/explure/model/route/RouteDTO.kt
+++ app/src/main/java/de/ur/explure/model/route/RouteDTO.kt
@@ -1,22 +1,23 @@
 package de.ur.explure.model.route
 
 import android.os.Parcelable
+import com.google.firebase.firestore.ServerTimestamp
 import de.ur.explure.model.landmark.LandmarkDTO
 import kotlinx.android.parcel.Parcelize
 import java.util.Date
-import kotlin.collections.ArrayList
 
 @Parcelize
 data class RouteDTO(
     var authorId: String,
     var category: String,
-    var date: Date,
     var title: String,
     var description: String,
     var distance: Double,
     var duration: Double,
     var thumbnail: String?,
-    var landMarks: ArrayList<LandmarkDTO> = ArrayList()
+    var landMarks: MutableList<LandmarkDTO> = mutableListOf(),
+    @ServerTimestamp
+    var createdAt: Date? = null,
 ) : Parcelable {
 
     fun addLandMark(landMark: LandmarkDTO) {
