c7b0caf25f76717c4002c8a422033ab3fd7bece0
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-09 15:52:11 +0100

    Update User and UserDTO models to fit FirebaseStore serialization/deserialization requirements

---
+++ app/src/main/java/de/ur/explure/config/UserDocumentConfig.kt
@@ -0,0 +1,11 @@
+package de.ur.explure.config
+
+object UserDocumentConfig {
+    const val EMAIL_KEY = "email"
+    const val NAME_KEY = "name"
+    const val PROFILE_PICTURE_KEY = "profile_picture"
+    const val CREATED_ROUTES_KEY = "created_routes"
+    const val FAVOURITE_ROUTES_KEY = "favourite_routes"
+    const val ACTIVE_ROUTES_KEY = "active_routes"
+    const val FINISHED_ROUTES_KEY = "finished_routes"
+}
--- app/src/main/java/de/ur/explure/model/user/User.kt
+++ app/src/main/java/de/ur/explure/model/user/User.kt
@@ -1,16 +1,40 @@
 package de.ur.explure.model.user
 
 import android.os.Parcelable
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.PropertyName
+import de.ur.explure.config.UserDocumentConfig.ACTIVE_ROUTES_KEY
+import de.ur.explure.config.UserDocumentConfig.CREATED_ROUTES_KEY
+import de.ur.explure.config.UserDocumentConfig.EMAIL_KEY
+import de.ur.explure.config.UserDocumentConfig.FAVOURITE_ROUTES_KEY
+import de.ur.explure.config.UserDocumentConfig.FINISHED_ROUTES_KEY
+import de.ur.explure.config.UserDocumentConfig.NAME_KEY
+import de.ur.explure.config.UserDocumentConfig.PROFILE_PICTURE_KEY
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class User(
-    val id: String,
-    val email: String,
-    val name: String,
-    val profilePictureUrl: String?,
-    val activeRoutes: List<String> = emptyList(),
-    val createdRotes: List<String> = emptyList(),
-    val favouriteRoutes: List<String> = emptyList(),
-    val finishedRoutes: List<String> = emptyList()
+    @DocumentId
+    val id: String = "",
+    @get:PropertyName(EMAIL_KEY)
+    @set:PropertyName(EMAIL_KEY)
+    var email: String = "",
+    @get:PropertyName(NAME_KEY)
+    @set:PropertyName(NAME_KEY)
+    var name: String = "",
+    @get:PropertyName(PROFILE_PICTURE_KEY)
+    @set:PropertyName(PROFILE_PICTURE_KEY)
+    var profilePictureUrl: String = "",
+    @get:PropertyName(ACTIVE_ROUTES_KEY)
+    @set:PropertyName(ACTIVE_ROUTES_KEY)
+    var activeRoutes: List<String> = emptyList(),
+    @get:PropertyName(CREATED_ROUTES_KEY)
+    @set:PropertyName(CREATED_ROUTES_KEY)
+    var createdRotes: List<String> = emptyList(),
+    @get:PropertyName(FAVOURITE_ROUTES_KEY)
+    @set:PropertyName(FAVOURITE_ROUTES_KEY)
+    var favouriteRoutes: List<String> = emptyList(),
+    @get:PropertyName(FINISHED_ROUTES_KEY)
+    @set:PropertyName(FINISHED_ROUTES_KEY)
+    var finishedRoutes: List<String> = emptyList()
 ) : Parcelable
