95c2b675f21404596bdc9d9662521575807a2dba
Pascal Lindemann <38719784+PascalLindemann@users.noreply.github.com>
2021-02-10 03:20:04 +0100

    Add Profile Layout

--- app/src/main/java/de/ur/explure/views/ProfileFragment.kt
+++ app/src/main/java/de/ur/explure/views/ProfileFragment.kt
@@ -1,6 +1,20 @@
 package de.ur.explure.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import de.ur.explure.R
 
-class ProfileFragment : Fragment(R.layout.fragment_profile)
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
