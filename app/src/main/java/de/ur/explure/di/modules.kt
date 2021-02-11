327d999c479676b3d0e5d52dc032d99551717221
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-11 11:55:09 +0100

    Move Bottom Navigation to BottomNavFragment and implement needed initialization methods for bottom nav and toolbar in BottomNavFragment

--- app/src/main/java/de/ur/explure/di/modules.kt
+++ app/src/main/java/de/ur/explure/di/modules.kt
@@ -2,13 +2,14 @@ package de.ur.explure.di
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import de.ur.explure.navigation.AppRouter
-import de.ur.explure.repository.AuthenticationRepository
+import de.ur.explure.navigation.MainAppRouter
+import de.ur.explure.navigation.StateAppRouter
 import de.ur.explure.viewmodel.AuthenticationViewModel
 import de.ur.explure.repository.user.UserRepositoryImpl
 import de.ur.explure.services.FireStoreInstance
 import de.ur.explure.services.FirebaseAuthService
-import de.ur.explure.viewmodel.MainViewModel
+import de.ur.explure.viewmodel.BottomNavViewModel
+import de.ur.explure.viewmodel.StateViewModel
 import de.ur.explure.viewmodel.TestViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -19,14 +20,15 @@ import org.koin.dsl.module
  */
 
 val mainModule = module {
-    single { AppRouter() }
+    single { MainAppRouter() }
+    single { StateAppRouter() }
     single { FirebaseAuth.getInstance() }
-    single { AuthenticationRepository(get()) }
     factory { FirebaseFirestore.getInstance() }
     factory { FireStoreInstance(get()) }
     single { FirebaseAuthService(get()) }
     single { UserRepositoryImpl(get(), get()) }
-    viewModel { AuthenticationViewModel(get()) }
+    viewModel { AuthenticationViewModel(get(), get()) }
     viewModel { TestViewModel(get(), get(), get()) }
-    viewModel { MainViewModel(get()) }
+    viewModel { StateViewModel() }
+    viewModel { BottomNavViewModel() }
 }
