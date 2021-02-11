9714a6d182ea37270b998ce3d5c7f2575fd6e389
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-11 11:53:21 +0100

    Separate AppRouter in State and MainAppRouter to improve app navigation

---
+++ app/src/main/java/de/ur/explure/navigation/StateAppRouter.kt
@@ -0,0 +1,56 @@
+package de.ur.explure.navigation
+
+import androidx.navigation.NavController
+import de.ur.explure.R
+
+/**
+ * Navigation router used for login and register and switching between auth states.
+ *
+ */
+
+class StateAppRouter {
+
+    private lateinit var navController: NavController
+
+    /**
+     * Initializes the navController
+     *
+     * @param navController Current NavController
+     */
+
+    fun initializeNavController(navController: NavController) {
+        this.navController = navController
+        this.navController.setGraph(R.navigation.nav_graph_auth)
+    }
+
+    /**
+     * Sets the Navigation graph to auth graph and navigates to LoginFragment while removing
+     * stateFragment from Backstack
+     */
+
+    fun navigateToLogin() {
+        navController.setGraph(R.navigation.nav_graph_auth)
+        navController.navigate(R.id.navigateAndPopBackStackToLoginFragment)
+    }
+
+    /**
+     * Navigates to BottomNavFragment to init main application
+     *
+     */
+
+    fun navigateToMainApp() {
+        navController.setGraph(R.navigation.nav_graph_host)
+    }
+
+    fun navigateToRegister() {
+        navController.navigate(R.id.navigateToRegister)
+    }
+
+    fun navigateUp(): Boolean {
+        return if (this::navController.isInitialized) {
+            navController.navigateUp()
+        } else {
+            false
+        }
+    }
+}
