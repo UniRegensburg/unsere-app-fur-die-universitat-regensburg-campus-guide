37079f8fd1c4804a8bea3915fa0f255f31b167f5
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-11 15:42:04 +0100

    Add KDoc

--- app/src/main/java/de/ur/explure/repository/rating/RatingRepositoryImpl.kt
+++ app/src/main/java/de/ur/explure/repository/rating/RatingRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package de.ur.explure.repository.rating
 
 import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FieldValue
 import de.ur.explure.config.ErrorConfig
+import de.ur.explure.config.RatingDocumentConfig.DATE_FIELD
 import de.ur.explure.config.RatingDocumentConfig.RATING_FIELD
 import de.ur.explure.extensions.await
 import de.ur.explure.model.rating.Rating
@@ -16,6 +18,16 @@ class RatingRepositoryImpl(
     private val fireStore: FireStoreInstance
 ) : RatingRepository {
 
+    /**
+     * Creates a new rating document in FireStore with the given information.
+     *
+     * @param ratingDTO [RatingDTO] object containing the rating value and the route id.
+     * @return On Success: Returns [FirebaseResult.Success] with empty return\
+     * On Failure: Returns [FirebaseResult.Error] with exception\
+     * On Cancellation: Returns [FirebaseResult.Canceled] with exception
+     *
+     */
+
     override suspend fun addRatingToFireStore(ratingDTO: RatingDTO): FirebaseResult<Void> {
         return try {
             val userId = firebaseAuth.getCurrentUserId() ?: return ErrorConfig.NO_USER_RESULT
@@ -24,6 +36,16 @@ class RatingRepositoryImpl(
             FirebaseResult.Error(exception)
         }
     }
+
+    /**
+     * Retrieves a Rating object from the FireStore database.
+     *
+     * @param ratingId [String] object with the id of the rating.
+     * @return On Success: Returns [FirebaseResult.Success] with a [Rating] object as data.\
+     * On Failure: Returns [FirebaseResult.Error] with exception.\
+     * On Cancellation: Returns [FirebaseResult.Canceled] with exception.
+     *
+     */
 
     override suspend fun getRating(ratingId: String): FirebaseResult<Rating> {
         return try {
@@ -42,6 +64,16 @@ class RatingRepositoryImpl(
         }
     }
 
+    /**
+     * Retrieves a List of Rating objects from the FireStore database.
+     *
+     * @param ratingIds [List] of [String] with the ids which should be retrieved.
+     * @return On Success: Returns [FirebaseResult.Success] with a [List] of [Rating] as data.\
+     * On Failure: Returns [FirebaseResult.Error] with exception.\
+     * On Cancellation: Returns [FirebaseResult.Canceled] with exception.
+     *
+     */
+
     override suspend fun getRatingList(ratingIds: List<String>): FirebaseResult<List<Rating>> {
         return try {
             when (val resultDocumentSnapshot =
@@ -59,14 +91,40 @@ class RatingRepositoryImpl(
         }
     }
 
+    /**
+     * Edits a rating's value and updates its timestamp
+     *
+     * @param ratingId [String] object with the id of the rating.
+     * @param ratingValue [Int] object with the rating value.
+     * @return On Success: Returns [FirebaseResult.Success] with an empty response.\
+     * On Failure: Returns [FirebaseResult.Error] with exception.\
+     * On Cancellation: Returns [FirebaseResult.Canceled] with exception.
+     *
+     */
+
     override suspend fun editRating(ratingId: String, ratingValue: Int): FirebaseResult<Void> {
         return try {
-            fireStore.ratingCollection.document(ratingId).update(mapOf(RATING_FIELD to ratingValue))
+            fireStore.ratingCollection.document(ratingId).update(
+                mapOf(
+                    RATING_FIELD to ratingValue,
+                    DATE_FIELD to FieldValue.serverTimestamp()
+                )
+            )
                 .await()
         } catch (exception: Exception) {
             FirebaseResult.Error(exception)
         }
     }
+
+    /**
+     * Deletes a rating from the FireStore database.
+     *
+     * @param ratingId [String] object with the id of the rating.
+     * @return On Success: Returns [FirebaseResult.Success] with an empty response.\
+     * On Failure: Returns [FirebaseResult.Error] with exception.\
+     * On Cancellation: Returns [FirebaseResult.Canceled] with exception.
+     *
+     */
 
     override suspend fun deleteRating(ratingId: String): FirebaseResult<Void> {
         return try {
