7c556fcadd93ed0f24661e1f6da78a50a6b636ad
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-11 11:50:41 +0100

    Move Auth operation to AuthService

--- app/src/main/java/de/ur/explure/services/FirebaseAuthService.kt
+++ app/src/main/java/de/ur/explure/services/FirebaseAuthService.kt
@@ -1,36 +1,64 @@
 package de.ur.explure.services
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import de.ur.explure.extensions.await
 import de.ur.explure.utils.FirebaseResult
 
-// Just for testing
 @Suppress("TooGenericExceptionCaught")
 class FirebaseAuthService(private val firebaseAuth: FirebaseAuth) {
 
-    suspend fun logInUserWithEmail(
-        email: String,
-        password: String
-    ): FirebaseResult<FirebaseUser?> {
-        try {
-            return when (val resultDocumentSnapshot =
-                firebaseAuth.signInWithEmailAndPassword(email, password).await()) {
-                is FirebaseResult.Success -> {
-                    val firebaseUser = resultDocumentSnapshot.data.user
-                    FirebaseResult.Success(firebaseUser)
-                }
-                is FirebaseResult.Error -> {
-                    FirebaseResult.Error(resultDocumentSnapshot.exception)
-                }
-                is FirebaseResult.Canceled -> {
-                    FirebaseResult.Canceled(resultDocumentSnapshot.exception)
-                }
-            }
-        } catch (exception: Exception) {
-            return FirebaseResult.Error(exception)
+    private val user: MutableLiveData<FirebaseUser> = MutableLiveData<FirebaseUser>()
+    val currentUser: LiveData<FirebaseUser> = user
+
+    init {
+        setAuthStateListener()
+    }
+
+    private fun setAuthStateListener() {
+        firebaseAuth.addAuthStateListener { auth ->
+            user.postValue(auth.currentUser)
         }
     }
 
-    fun getCurrentUserId(): String? = firebaseAuth.currentUser?.uid
+    suspend fun registerUser(email: String, password: String): FirebaseResult<AuthResult> {
+        return firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+    }
+
+    fun signIn(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    user.postValue(firebaseAuth.currentUser)
+                }
+            }
+    }
+
+    fun resetPassword(email: String) {
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful) {
+                    // Toast.makeText(this, "Email gesendet", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun signInAnonymously() {
+        firebaseAuth.signInAnonymously()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    user.postValue(firebaseAuth.currentUser)
+                }
+            }
+    }
+
+    fun logout() {
+        firebaseAuth.signOut()
+    }
+
+    fun getCurrentUserId(): String? = currentUser.value?.uid
 }
