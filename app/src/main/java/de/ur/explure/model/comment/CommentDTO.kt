168678e0435cab88b85762852755463704628f95
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-17 17:39:43 +0100

    Adjust Comment model to fit Firestore serialization

--- app/src/main/java/de/ur/explure/model/comment/CommentDTO.kt
+++ app/src/main/java/de/ur/explure/model/comment/CommentDTO.kt
@@ -1,14 +1,25 @@
 package de.ur.explure.model.comment
 
 import android.os.Parcelable
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ServerTimestamp
+import de.ur.explure.config.CommentDocumentConfig.ANSWERS_FIELD
+import de.ur.explure.config.CommentDocumentConfig.AUTHOR_ID_FIELD
+import de.ur.explure.config.CommentDocumentConfig.DATE_FIELD
+import de.ur.explure.config.CommentDocumentConfig.MESSAGE_FIELD
 import java.util.Date
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CommentDTO(
     var message: String,
-    var authorId: String,
-    @ServerTimestamp
-    val createdAt: Date? = null,
-) : Parcelable
+) : Parcelable {
+
+    fun toMap(userId: String): Map<String, Any>{
+        return mapOf(
+            MESSAGE_FIELD to message,
+            AUTHOR_ID_FIELD to userId,
+            DATE_FIELD to FieldValue.serverTimestamp(),
+        )
+    }
+}
