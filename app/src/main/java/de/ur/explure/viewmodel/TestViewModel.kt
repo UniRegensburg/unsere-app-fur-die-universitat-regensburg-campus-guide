15bcba8407b6fea1c79098e17def23d1b82014d9
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-02-20 10:11:52 +0100

    Adjust TestViewModel

--- app/src/main/java/de/ur/explure/viewmodel/TestViewModel.kt
+++ app/src/main/java/de/ur/explure/viewmodel/TestViewModel.kt
@@ -7,6 +7,8 @@ import de.ur.explure.navigation.MainAppRouter
 import com.google.firebase.firestore.GeoPoint
 import de.ur.explure.model.route.RouteDTO
 import de.ur.explure.model.waypoint.WayPointDTO
+import de.ur.explure.repository.route.RouteRepositoryImpl
+import de.ur.explure.repository.user.UserRepositoryImpl
 import de.ur.explure.services.FirebaseAuthService
 import kotlinx.coroutines.launch
 
@@ -21,7 +23,7 @@ class TestViewModel(
 
     fun loginUser() {
         viewModelScope.launch {
-            authService.logInUserWithEmail("testUser@test.de", "Password1")
+            authService.signIn("testUser@test.de", "Password1")
         }
     }
 
