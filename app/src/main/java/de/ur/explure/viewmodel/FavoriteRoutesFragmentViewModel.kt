bfcf0a110c0631b7c8b414863687ce58e38c7317
Pascal Lindemann <38719784+PascalLindemann@users.noreply.github.com>
2021-02-20 03:58:52 +0100

    Implement ViewModel for FavoriteRoutesFragment

---
+++ app/src/main/java/de/ur/explure/viewmodel/FavoriteRoutesFragmentViewModel.kt
@@ -0,0 +1,46 @@
+package de.ur.explure.viewmodel
+
+import android.graphics.BitmapFactory
+import android.os.NetworkOnMainThreadException
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.ur.explure.repository.user.UserRepositoryImpl
+import de.ur.explure.utils.FirebaseResult
+import kotlinx.coroutines.launch
+
+class FavoriteRoutesFragmentViewModel(
+    private val userRepo: UserRepositoryImpl
+) : ViewModel() {
+
+    fun setUserName(textView: TextView) {
+        textView.text = ""
+        viewModelScope.launch() {
+            val userInfo = userRepo.getUserInfo()
+            when (userInfo) {
+                is FirebaseResult.Success -> {
+                    textView.text = userInfo.data.name
+                }
+            }
+        }
+    }
+
+    fun setProfilePicture(imageView: ImageView) {
+        viewModelScope.launch() {
+            val userInfo = userRepo.getUserInfo()
+            when (userInfo) {
+                is FirebaseResult.Success -> {
+                    try {
+                        val inp = java.net.URL(userInfo.data.profilePictureUrl).openStream()
+                        val image = BitmapFactory.decodeStream(inp)
+                        imageView.setImageBitmap(image)
+                    } catch (exception: NetworkOnMainThreadException) {
+                        Log.e("TAG", "" + exception)
+                    }
+                }
+            }
+        }
+    }
+}
