81ac1e07de94e88c832d8af985cf6886b213886f
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2021-03-01 12:05:04 +0100

    Add some custom animations

--- app/src/main/java/de/ur/explure/utils/Utils.kt
+++ app/src/main/java/de/ur/explure/utils/Utils.kt
@@ -2,8 +2,12 @@ package de.ur.explure.utils
 
 import android.content.Context
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import android.widget.ListAdapter
+import androidx.fragment.app.Fragment
+import de.ur.explure.R
 
 /**
  * Returns the width of the longest item in a list.
@@ -36,4 +40,27 @@ fun measureContentWidth(context: Context, adapter: ListAdapter): Int {
         }
     }
     return maxWidth
+}
+
+/**
+ * This slides a given view out to the right.
+ */
+fun Fragment.slideOutView(v: View) {
+    val slideOutAnim: Animation =
+        AnimationUtils.loadAnimation(requireActivity(), R.anim.slide_out_to_right)
+    v.startAnimation(slideOutAnim)
+
+    // https://stackoverflow.com/questions/4728908/android-view-with-view-gone-still-receives-ontouch-and-onclick
+    v.visibility = View.GONE
+    v.clearAnimation()
+}
+
+/**
+ * This slides a given view in from the right.
+ */
+fun Fragment.slideInView(v: View) {
+    val slideInAnim: Animation =
+        AnimationUtils.loadAnimation(requireActivity(), R.anim.slide_in_from_right)
+    v.startAnimation(slideInAnim)
+    v.clearAnimation()
 }
