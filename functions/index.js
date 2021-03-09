5cdcad394a630deb409514aeb24fc8d20a488cb5
Andreas Filipiuk <andreas.filipiuk@anybill.de>
2021-03-09 21:11:16 +0100

    Add functions to calculate current Rating of each Route when Ratings are added/deleted

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,9 @@ admin.initializeApp();
 // Get functions from different files
 const commentCount = require("./functions/commentCount");
 const wayPointCount = require("./functions/wayPointCount");
+const ratingCalculator = require("./functions/ratingCalculator");
 
 // Export functions to firebase
 exports.commentCount = commentCount.commentCount;
 exports.wayPointCount = wayPointCount.wayPointCount;
+exports.ratingCalculator = ratingCalculator.ratingCalculator;
