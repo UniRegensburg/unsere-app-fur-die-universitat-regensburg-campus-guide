e981150f20c93f86c3443f772ee2f9faf3870684
Pascal Lindemann <38719784+PascalLindemann@users.noreply.github.com>
2021-03-16 03:21:58 +0100

    Integrate improved route items in created/favorite routes list

--- app/src/main/java/de/ur/explure/adapter/ProfileRouteAdapter.kt
+++ app/src/main/java/de/ur/explure/adapter/ProfileRouteAdapter.kt
@@ -3,8 +3,15 @@ package de.ur.explure.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.firebase.storage.FirebaseStorage
+import de.ur.explure.GlideApp
+import de.ur.explure.R
 import de.ur.explure.databinding.RouteElementBinding
 import de.ur.explure.model.route.Route
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.java.KoinJavaComponent.inject
 
 class ProfileRouteAdapter(
     private val onItemClickListener: OnItemClickListener,
@@ -28,15 +35,38 @@ class ProfileRouteAdapter(
     override fun getItemCount() = routeList.size
 
     class ProfileRouteViewHolder private constructor(private val binding: RouteElementBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(binding.root), KoinComponent {
+
+        private val fireStorage: FirebaseStorage by inject()
 
         fun bind(
             currentRouteItem: Route,
             onClickListener: OnItemClickListener,
             onLongClickListener: OnItemLongClickListener
         ) {
-            binding.routeName.text = currentRouteItem.title
-            binding.routeTimestamp.text = currentRouteItem.createdAt.toString()
+            if (currentRouteItem.thumbnailUrl.isNotEmpty()) {
+                try {
+                    val gsReference = fireStorage.getReferenceFromUrl(currentRouteItem.thumbnailUrl)
+                    GlideApp.with(itemView)
+                        .load(gsReference)
+                        .error(R.drawable.map_background)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .into(binding.ivRouteThumbnail)
+                } catch (_: Exception) {
+                }
+            }
+
+            binding.tvRouteTitle.text = currentRouteItem.title
+            binding.tvRouteDescription.text = currentRouteItem.description
+            binding.tvRatingCount.text = itemView.context.resources.getString(
+                R.string.route_item_rating, currentRouteItem.rating.size)
+            binding.tvDistance.text = itemView.context.resources.getString(
+                R.string.route_item_distance, currentRouteItem.distance.toInt())
+            binding.tvDuration.text = itemView.context.resources.getString(
+                R.string.route_item_duration, currentRouteItem.duration.toInt())
+            binding.tvWayPointCount.text = currentRouteItem.wayPointCount.toString()
+            binding.tvComment.text = currentRouteItem.commentCount.toString()
+            binding.ratingBarRoute.rating = currentRouteItem.currentRating.toFloat()
 
             binding.root.setOnClickListener {
                 val position: Int = adapterPosition
