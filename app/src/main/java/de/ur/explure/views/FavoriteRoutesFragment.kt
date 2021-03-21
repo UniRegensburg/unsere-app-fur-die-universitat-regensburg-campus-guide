813a19a1498d8ade2751f6fd28b259a5bb0184e6
Pascal Lindemann <38719784+PascalLindemann@users.noreply.github.com>
2021-03-21 01:18:37 +0100

    Improve UI of profile subpages

--- app/src/main/java/de/ur/explure/views/FavoriteRoutesFragment.kt
+++ app/src/main/java/de/ur/explure/views/FavoriteRoutesFragment.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.crazylegend.viewbinding.viewBinding
+import com.google.firebase.storage.FirebaseStorage
+import de.ur.explure.GlideApp
 import de.ur.explure.R
 import de.ur.explure.adapter.ProfileRouteAdapter
 import de.ur.explure.databinding.FragmentFavoriteRoutesBinding
 import de.ur.explure.model.route.Route
 import de.ur.explure.viewmodel.FavoriteRoutesViewModel
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteRoutesFragment : Fragment(R.layout.fragment_favorite_routes),
@@ -21,10 +25,15 @@ class FavoriteRoutesFragment : Fragment(R.layout.fragment_favorite_routes),
     private val viewModel: FavoriteRoutesViewModel by viewModel()
     private lateinit var adapter: ProfileRouteAdapter
 
+    private var userId: String = ""
+
+    private val fireStorage: FirebaseStorage by inject()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initializeAdapter()
 
+        observeUserModel()
         observeRouteModel()
         viewModel.getUserInfo()
         viewModel.getFavoriteRoutes()
@@ -35,6 +44,27 @@ class FavoriteRoutesFragment : Fragment(R.layout.fragment_favorite_routes),
         binding.favoriteRoutesRecyclerView.adapter = adapter
         binding.favoriteRoutesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.favoriteRoutesRecyclerView.setHasFixedSize(true)
+    }
+
+    private fun observeUserModel() {
+        viewModel.user.observe(viewLifecycleOwner, { user ->
+            if (user != null) {
+                binding.userNameTextView.text = user.name
+                userId = user.id
+                if (user.profilePictureUrl.isNotEmpty()) {
+                    try {
+                        val gsReference =
+                                fireStorage.getReferenceFromUrl(user.profilePictureUrl)
+                        GlideApp.with(requireContext())
+                                .load(gsReference)
+                                .error(R.drawable.user_profile_picture)
+                                .transition(DrawableTransitionOptions.withCrossFade())
+                                .into(binding.profilePicture)
+                    } catch (_: Exception) {
+                    }
+                }
+            }
+        })
     }
 
     private fun observeRouteModel() {
